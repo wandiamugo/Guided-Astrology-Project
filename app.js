bbc253664ae070e0964f338d2a7f0b7fd891e683
Wandia E Mugo <wwandia173@gmail.com>
2022-06-24 11:16:56 +0300

    Function Declaration JS

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    //recording date for local storage
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+
+    today = mm + '/' + dd + '/' + yyyy;
+
+    console.log("today's date: " + today);
+
+    //variable to set up submit function based on horoscope signs
+    let userInput = $("a");
+
+    //local storage hold for previous sign
+    let lastselectSign = localStorage.getItem("last selected sign: ");
+    console.log("last selected sign brought back: " + lastselectSign);
+
+    var lastHoroText = localStorage.getItem("text for last horoscope text");
+    console.log("last selected sign with date of horoscope and the horoscope: " + lastHoroText);
+
+    //put localstorage to page in text
+    $("#last-select").text("Last selected Sign: " + lastselectSign);
+
+    //if last selected sign has no local storage it will hide the text on the website page
+    if (lastselectSign === null) {
+        $("#last-select").attr("class", "hide");
+    };
+});  
